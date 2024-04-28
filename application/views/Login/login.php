afe76b6a6f74767eb7c0b638de751f8b476917ea
novisetiannn <novisetiani55@gmail.com>
2024-04-28 23:25:23 +0700

    projectpblif23-2-2

---
+++ application/views/Login/login.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+
+<head>
+	<!-- Favicons -->
+	<link href="<?= base_url()?>vendor/image/logo.png" rel="icon">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="<?= base_url('vendor/'); ?>css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('vendor/'); ?>Image/fontawesome/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('vendor/'); ?>Image/fontawesome/css/fontawesome.min.css">
+	<link rel="stylesheet" href="<?= base_url('vendor/'); ?>style(login).css">
+
+	<!-- JavaScript -->
+	<script src="<?= base_url('vendor/'); ?>js/jquery-3.7.0.min.js"></script>
+	<script src="<?= base_url('vendor/'); ?>js/bootstrap.min.js"></script>
+	<title>Login</title>
+</head>
+
+<body>
+	<div class="global-container">
+		<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
+			<form class="login-form" action="<?php echo base_url('Login/ceklogin'); ?>" method="post">
+				<p align="center" style="color: white; font-size: 40px; font-weight: lighter;">SEMB-OWS Tracking</p>
+				<div class="card container col-5 pt-4 pb-5" id="main">
+					<div class="card-body">
+						<div class="mb-3">
+							<input type="text" class="form-control" name="username" id="username" placeholder="SESA ID">
+						</div>
+						<div class="mb-3">
+							<input type="password" name="password" class="form-control" id="password" placeholder="Password">
+						</div>
+						<div class="mb-3" align="center">
+							<button type="submit" class="form-control btn btn-primary"><i class="fa-solid fa-right-to-bracket"></i> Sign In</button>
+						</div>
+						<img alt="" src="vendor/Image/schneider-electric.png" width="100%">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+
+</html>
